0d47305e03bf22ec1b209910d012441a6470df50
crazyyy <crazyyy@gmail.com>
2017-01-12 18:10:10 +0200

    before new group

--- wordpress/wp-content/themes/wp-goldiesgym/page-group.php
+++ wordpress/wp-content/themes/wp-goldiesgym/page-group.php
@@ -2,96 +2,227 @@
 
   <?php $current_page_id = get_the_ID(); ?>
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-  <section class="group-classes">
-    <div class="container">
-      <div class="row">
-        <div class="section-tittle">
-          <h2>ГРУППОВЫЕ ЗАНЯТИЯ</h2>
-        </div>
-        <!-- end section-tittle -->
-        <div class="section-body">
-          <div class="group-classes__block">
-            <div class="group-classes__block-tittle">
-              <h3>ПОПУЛЯРНЫЕ ПАКЕТЫ</h3>
-            </div>
-            <!-- end group-classes__block-tittle -->
-
-            <?php if( have_rows('abonements') ): while ( have_rows('abonements') ) : the_row();?>
-              <div class="group-classes__popuar">
-                <div class="col-md-3">
-                  <h3 class="group-classes__tittle"><?php the_sub_field('groupe_title'); ?></h3>
-                  <p class="group-classes__date"><?php the_sub_field('group_subtitle'); ?></p>
-                </div>
-                <div class="col-sm-4 col-md-3 group-classes__card-wrap">
-                  <div class="group-classes__card">
-                    <p><?php the_sub_field('block_one_title'); ?></p>
-                    <span><?php the_sub_field('block_one_price'); ?></span>
-                    <button type="button" class="btn btn-inf" data-container="body" data-toggle="popover" data-placement="top" data-content="+ групповые"><i class="ic ic-info"></i></button>
-                  </div>
-                </div>
-                <!-- end group-classes__card-wrap -->
-                <div class="col-sm-4 col-md-3 group-classes__card-wrap">
-                  <div class="group-classes__card">
-                    <p><?php the_sub_field('block_two_title'); ?></p>
-                    <span><?php the_sub_field('block_two_price'); ?></span>
-                    <button type="button" class="btn btn-inf" data-container="body" data-toggle="popover" data-placement="top" data-content="+ групповые"><i class="ic ic-info"></i></button>
-                  </div>
-                </div>
-                <!-- end group-classes__card-wrap -->
-                <div class="col-sm-4 col-md-3 group-classes__card-wrap">
-                  <div class="group-classes__card">
-                    <p><?php the_sub_field('block_three_title'); ?></p>
-                    <span><?php the_sub_field('block_three_price'); ?></span>
-                    <button type="button" class="btn btn-inf" data-container="body" data-toggle="popover" data-placement="top" data-content="+ групповые"><i class="ic ic-info"></i></button>
-                  </div>
-                </div>
-                <!-- end group-classes__card-wrap -->
-                <div class="clearfix"></div>
-              </div><!-- end group-classes__popuar -->
-            <?php endwhile; endif; ?>
-
+    <section class="about-us">
+      <div class="container">
+        <div class="row">
+          <div class="section-tittle">
+            <h2><?php the_title(); ?></h2>
           </div>
-          <!-- end group-classes__block -->
-          <div class="group-classes__block">
-            <div class="group-classes__block-tittle group-classes__block-tittle--all">
-              <h3>ВСЕ УСЛУГИ</h3>
-            </div>
-            <div class="personal-classes__block">
-              <div class="col-md-3">
-              <h3 class="group-classes__tittle">ПЕРСОНАЛЬНЫЕ <br>ТРЕНИРОВКИ</h3>
+          <!-- end section-tittle -->
+        </div>
+      </div>
+      <div class="about-us__descr">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 about-club__text">
+              <?php the_content(); ?>
+              <div class="col-xs-12 col-sm-4 about-club__block">
+                <div class="about-club__block-icon">
+                  <i class="ic ic-power"></i>
+                </div>
+                <h3>СИЛА</h3>
               </div>
-              <div class="col-md-9">
-                <table class="all-services">
-                  <tbody>
-                    <?php if( have_rows('services') ): while ( have_rows('services') ) : the_row();?>
-                      <tr>
-                        <td><?php the_sub_field('services_title'); ?></td>
-                        <td><?php the_sub_field('services_time'); ?></td>
-                        <td><?php the_sub_field('services_cost'); ?></td>
-                      </tr>
-                    <?php endwhile; endif; ?>
-                  </tbody>
-                </table>
+              <div class="col-xs-12 col-sm-4 about-club__block">
+                <div class="about-club__block-icon">
+                  <i class="ic ic-energy"></i>
+                </div>
+                <h3>ЭНЕРГИЯ</h3>
+              </div>
+              <div class="col-xs-12 col-sm-4 about-club__block">
+                <div class="about-club__block-icon">
+                  <i class="ic ic-honor"></i>
+                </div>
+                <h3>СМЕЛОСЬ</h3>
               </div>
               <div class="clearfix"></div>
-            </div><!-- end personal-classes__block -->
-
+            </div>
+            <div class="clearfix"></div>
           </div>
-          <!-- end group-classes__block -->
         </div>
-        <!-- end section-body -->
+        <img class="bg-about-us" src="<?php echo get_template_directory_uri(); ?>/img/bg-about.png" alt="about">
       </div>
-    </div>
-  </section>
-  <!-- end section.group-classes -->
+      <!-- end about-us__descr -->
+      <div class="about-us__advantages">
+        <div class="container">
+          <div class="row">
+            <div class="section-tittle--left">
+              <h2>ПРЕИМУЩЕСТВА</h2>
+            </div>
+            <!-- end section-tittle--left -->
+            <div class="about-us__advantages-wrap">
+
+              <?php $i = 1; if( have_rows('advantages') ): while ( have_rows('advantages') ) : the_row();?>
+                <div class="col-sm-6 col-md-4 about-us__advantages-block">
+                  <div class="about-us__advantages-block-inner">
+                    <span><?php echo $i; ?></span>
+                    <h3><?php the_sub_field('advantage_title'); ?></h3>
+                    <p><?php the_sub_field('advantages_content'); ?></p>
+                  </div>
+                </div><!-- end about-us__advantages-block -->
+              <?php $i++; endwhile; endif; ?>
+
+              <div class="clearfix"></div>
+            </div>
+            <!-- end about-us__advantages-wrap -->
+          </div>
+        </div>
+      </div>
+      <!-- end about-us__advantages -->
+    </section>
+    <!-- end section.about-us -->
+    <section class="about-us__gallery">
+      <div class="container">
+        <div class="row">
+          <div class="section-tittle--left">
+            <h2>ФОТОГАЛЕРЕЯ</h2>
+            <ul class="nav nav-tabs nav-tabs--gallery">
+              <li class="active"><a href="#interior" data-toggle="tab">ИНТЕРЬЕР</a></li>
+              <li><a href="#apparatus" data-toggle="tab">ТРЕНАЖЕРЫ</a></li>
+            </ul>
+          </div>
+          <!-- end section-tittle--left -->
+        </div>
+      </div>
+      <!-- end container -->
+      <div class="tab-content gallery">
+        <div class="tab-pane fade in active" id="interior">
+          <div class="gallery__images">
+
+            <?php if( have_rows('gallery_first_block') ): while ( have_rows('gallery_first_block') ) : the_row();?>
+              <?php $image = get_sub_field('images'); if( !empty($image) ): ?>
+                <div class="col-xs-6 col-md-2 gallery__images-block">
+                  <a href="<?php echo $image['url']; ?>" data-toggle="lightbox" data-gallery="example-gallery">
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-fluid">
+                  </a>
+                </div>
+              <?php endif; ?>
+            <?php  endwhile; endif; ?>
+
+            <div class="clearfix"></div>
+          </div><!-- end gallery__images -->
+        </div>
+        <div class="tab-pane fade" id="apparatus">
+          <div class="gallery__images">
+
+            <?php if( have_rows('gallery_second') ): while ( have_rows('gallery_second') ) : the_row();?>
+              <?php $image = get_sub_field('image'); if( !empty($image) ): ?>
+                <div class="col-xs-6 col-md-2 gallery__images-block">
+                  <a href="<?php echo $image['url']; ?>" data-toggle="lightbox" data-gallery="example-gallery">
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-fluid">
+                  </a>
+                </div>
+              <?php endif; ?>
+            <?php  endwhile; endif; ?>
+
+            <div class="clearfix"></div>
+          </div>
+          <!-- end gallery__images -->
+        </div>
+        <div class="gallery__descr-wrap"></div>
+      </div>
+    </section>
+    <!-- end about-us__gallery -->
+    <section class="about-us__personal">
+      <div class="container">
+        <div class="row">
+          <div class="section-tittle--left">
+            <h2>КОМАНДА</h2>
+            <ul class="nav nav-tabs nav-tabs--gallery nav-tabs--personal">
+              <li class="active"><a href="#instructors" data-toggle="tab">ИНСТРУКТОРЫ</a></li>
+              <li><a href="#administration" data-toggle="tab">АДМИНИСТРАЦИЯ</a></li>
+            </ul>
+          </div>
+          <!-- end section-tittle--left -->
+          <div class="tab-content personal">
+            <div class="tab-pane fade in active" id="instructors">
+              <div class="slider-personal">
 
 
+                <?php if( have_rows('instructors') ): while ( have_rows('instructors') ) : the_row();?>
+                  <?php $image = get_sub_field('image'); if( !empty($image) ): ?>
+                    <div class="slider-personal__block">
+                      <div class="slider-personal__img">
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                      </div>
+                      <div class="slider-personal__text">
+                        <h3><?php the_sub_field('name'); ?></h3>
+                        <p><?php the_sub_field('position'); ?></p>
+                      </div>
+                    </div><!-- end slider-personal__block -->
 
+                  <?php endif; ?>
+                <?php  endwhile; endif; ?>
+
+              </div>
+              <!-- end slider-personal -->
+            </div>
+            <div class="tab-pane fade" id="administration">
+              <div class="slider-personal">
+
+                <?php if( have_rows('administration') ): while ( have_rows('administration') ) : the_row();?>
+                  <?php $image = get_sub_field('image'); if( !empty($image) ): ?>
+                    <div class="slider-personal__block">
+                      <div class="slider-personal__img">
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                      </div>
+                      <div class="slider-personal__text">
+                        <h3><?php the_sub_field('name'); ?></h3>
+                        <p><?php the_sub_field('position'); ?></p>
+                      </div>
+                    </div><!-- end slider-personal__block -->
+
+                  <?php endif; ?>
+                <?php  endwhile; endif; ?>
+
+              </div>
+              <!-- end slider-personal -->
+            </div>
+          </div>
+          <!-- end personal -->
+        </div>
+      </div>
+    </section><!-- end about-us__gallery -->
+
+    <?php /*
+    <section class="partners">
+      <div class="container">
+        <div class="row">
+          <div class="section-tittle">
+            <h2>НАШИ ПАРТНЕРЫ</h2>
+          </div>
+          <!-- end section-tittle -->
+          <div class="partners__slider">
+            <div class="partners__block">
+              <a href="#"></a>
+            </div>
+            <!-- end partners__block -->
+            <div class="partners__block">
+              <a href="#"></a>
+            </div>
+            <!-- end partners__block -->
+            <div class="partners__block">
+              <a href="#"></a>
+            </div>
+            <!-- end partners__block -->
+            <div class="partners__block">
+              <a href="#"></a>
+            </div>
+            <!-- end partners__block -->
+            <div class="partners__block">
+              <a href="#"></a>
+            </div>
+            <!-- end partners__block -->
+          </div>
+          <!-- end partners__slider -->
+        </div>
+      </div>
+    </section><!-- end section.partners -->
+    */ ?>
   <?php endwhile; else: ?>
     <section>
       <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
     </section>
   <?php endif; ?>
+
 
 <?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/wp-goldiesgym/page-price.php
@@ -0,0 +1,97 @@
+<?php /* Template Name: Price Page */ get_header(); ?>
+
+  <?php $current_page_id = get_the_ID(); ?>
+  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+  <section class="group-classes">
+    <div class="container">
+      <div class="row">
+        <div class="section-tittle">
+          <h2>ГРУППОВЫЕ ЗАНЯТИЯ</h2>
+        </div>
+        <!-- end section-tittle -->
+        <div class="section-body">
+          <div class="group-classes__block">
+            <div class="group-classes__block-tittle">
+              <h3>ПОПУЛЯРНЫЕ ПАКЕТЫ</h3>
+            </div>
+            <!-- end group-classes__block-tittle -->
+
+            <?php if( have_rows('abonements') ): while ( have_rows('abonements') ) : the_row();?>
+              <div class="group-classes__popuar">
+                <div class="col-md-3">
+                  <h3 class="group-classes__tittle"><?php the_sub_field('groupe_title'); ?></h3>
+                  <p class="group-classes__date"><?php the_sub_field('group_subtitle'); ?></p>
+                </div>
+                <div class="col-sm-4 col-md-3 group-classes__card-wrap">
+                  <div class="group-classes__card">
+                    <p><?php the_sub_field('block_one_title'); ?></p>
+                    <span><?php the_sub_field('block_one_price'); ?></span>
+                    <button type="button" class="btn btn-inf" data-container="body" data-toggle="popover" data-placement="top" data-content="+ групповые"><i class="ic ic-info"></i></button>
+                  </div>
+                </div>
+                <!-- end group-classes__card-wrap -->
+                <div class="col-sm-4 col-md-3 group-classes__card-wrap">
+                  <div class="group-classes__card">
+                    <p><?php the_sub_field('block_two_title'); ?></p>
+                    <span><?php the_sub_field('block_two_price'); ?></span>
+                    <button type="button" class="btn btn-inf" data-container="body" data-toggle="popover" data-placement="top" data-content="+ групповые"><i class="ic ic-info"></i></button>
+                  </div>
+                </div>
+                <!-- end group-classes__card-wrap -->
+                <div class="col-sm-4 col-md-3 group-classes__card-wrap">
+                  <div class="group-classes__card">
+                    <p><?php the_sub_field('block_three_title'); ?></p>
+                    <span><?php the_sub_field('block_three_price'); ?></span>
+                    <button type="button" class="btn btn-inf" data-container="body" data-toggle="popover" data-placement="top" data-content="+ групповые"><i class="ic ic-info"></i></button>
+                  </div>
+                </div>
+                <!-- end group-classes__card-wrap -->
+                <div class="clearfix"></div>
+              </div><!-- end group-classes__popuar -->
+            <?php endwhile; endif; ?>
+
+          </div>
+          <!-- end group-classes__block -->
+          <div class="group-classes__block">
+            <div class="group-classes__block-tittle group-classes__block-tittle--all">
+              <h3>ВСЕ УСЛУГИ</h3>
+            </div>
+            <div class="personal-classes__block">
+              <div class="col-md-3">
+              <h3 class="group-classes__tittle">ПЕРСОНАЛЬНЫЕ <br>ТРЕНИРОВКИ</h3>
+              </div>
+              <div class="col-md-9">
+                <table class="all-services">
+                  <tbody>
+                    <?php if( have_rows('services') ): while ( have_rows('services') ) : the_row();?>
+                      <tr>
+                        <td><?php the_sub_field('services_title'); ?></td>
+                        <td><?php the_sub_field('services_time'); ?></td>
+                        <td><?php the_sub_field('services_cost'); ?></td>
+                      </tr>
+                    <?php endwhile; endif; ?>
+                  </tbody>
+                </table>
+              </div>
+              <div class="clearfix"></div>
+            </div><!-- end personal-classes__block -->
+
+          </div>
+          <!-- end group-classes__block -->
+        </div>
+        <!-- end section-body -->
+      </div>
+    </div>
+  </section>
+  <!-- end section.group-classes -->
+
+
+
+  <?php endwhile; else: ?>
+    <section>
+      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
+    </section>
+  <?php endif; ?>
+
+<?php get_footer(); ?>
