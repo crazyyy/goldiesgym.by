5ada80b40741fe781549b94c7183e969602ea1f0
crazyyy <crazyyy@gmail.com>
2017-01-13 01:13:07 +0200

    fixes

--- wordpress/wp-content/themes/wp-goldiesgym/page-group.php
+++ wordpress/wp-content/themes/wp-goldiesgym/page-group.php
@@ -2,186 +2,171 @@
 
   <?php $current_page_id = get_the_ID(); ?>
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <section class="about-us">
-      <div class="container">
-        <div class="row">
-          <div class="section-tittle">
-            <h2><?php the_title(); ?></h2>
-          </div>
-          <!-- end section-tittle -->
+  <section class="shedule">
+    <div class="container">
+      <div class="row">
+        <div class="section-tittle">
+          <h2>ГРУППОВЫЕ ЗАНЯТИЯ</h2>
         </div>
-      </div>
-      <div class="about-us__descr">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 about-club__text">
-              <?php the_content(); ?>
-              <div class="col-xs-12 col-sm-4 about-club__block">
-                <div class="about-club__block-icon">
-                  <i class="ic ic-power"></i>
-                </div>
-                <h3>СИЛА</h3>
-              </div>
-              <div class="col-xs-12 col-sm-4 about-club__block">
-                <div class="about-club__block-icon">
-                  <i class="ic ic-energy"></i>
-                </div>
-                <h3>ЭНЕРГИЯ</h3>
-              </div>
-              <div class="col-xs-12 col-sm-4 about-club__block">
-                <div class="about-club__block-icon">
-                  <i class="ic ic-honor"></i>
-                </div>
-                <h3>СМЕЛОСЬ</h3>
-              </div>
-              <div class="clearfix"></div>
-            </div>
-            <div class="clearfix"></div>
+        <!-- end section-tittle -->
+        <div class="section-body">
+          <div class="shedule__text clearfix">
+            <?php the_content(); ?>
           </div>
-        </div>
-        <img class="bg-about-us" src="<?php echo get_template_directory_uri(); ?>/img/bg-about.png" alt="about">
-      </div>
-      <!-- end about-us__descr -->
-      <div class="about-us__advantages">
-        <div class="container">
-          <div class="row">
-            <div class="section-tittle--left">
-              <h2>ПРЕИМУЩЕСТВА</h2>
-            </div>
-            <!-- end section-tittle--left -->
-            <div class="about-us__advantages-wrap">
+          <!-- end shedule__text -->
+          <div class="shedule__slider-nav">
 
-              <?php $i = 1; if( have_rows('advantages') ): while ( have_rows('advantages') ) : the_row();?>
-                <div class="col-sm-6 col-md-4 about-us__advantages-block">
-                  <div class="about-us__advantages-block-inner">
-                    <span><?php echo $i; ?></span>
-                    <h3><?php the_sub_field('advantage_title'); ?></h3>
-                    <p><?php the_sub_field('advantages_content'); ?></p>
+            <?php if( have_rows('programs') ): while ( have_rows('programs') ) : the_row();?>
+              <div class="shedule__slider-nav-block">
+                <div class="slider-nav__img">
+                  <i class="ic ic-apple"></i>
+                </div>
+                <p><?php the_sub_field('short_title'); ?></p>
+              </div><!-- end shedule__slider-nav-block -->
+            <?php endwhile; endif; ?>
+
+          </div><!-- end shedule__slider-nav -->
+          <div class="shedule__slider">
+
+            <?php if( have_rows('programs') ): while ( have_rows('programs') ) : the_row();?>
+              <div class="shedule__slider-block">
+                <div class="col-sm-4 shedule__slider-img">
+                  <?php $image = get_sub_field('program_image'); ?>
+                  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                </div>
+                <div class="col-sm-6 col-md-4 shedule__slider-descr-wrap">
+                  <div class="shedule__slider-descr">
+                    <div class="shedule__slider-descr-text">
+                      <h3><?php the_sub_field('title'); ?></h3>
+                      <?php the_sub_field('description'); ?>
+                      <a href="#" class="more" data-toggle="modal" data-target="#myModal7">подробнее о программе...</a>
+                    </div>
+                    <!-- end shedule__slider-descr-text -->
+                    <div class="shedule__slider-descr-bot">
+                      <p><i class="ic ic-b_shedule"></i><?php the_sub_field('days'); ?></p>
+                      <p><i class="ic ic-b_time"></i><?php the_sub_field('time'); ?></p>
+                      <div class="clearfix"></div>
+                    </div>
+                    <!-- end shedule__slider-descr-bot -->
+                    <a href="<?php the_sub_field('link_test'); ?>" class="btn btn-solid btn-solid--romb">ЗАПИСАТЬСЯ НА ПРОБНОЕ ЗАНЯТИЕ<span></span></a>
+                    <a href="<?php the_sub_field('link'); ?>" class="btn btn-solid btn-solid--romb btn-mobile">ЗАПИСАТЬСЯ<span></span></a>
                   </div>
-                </div><!-- end about-us__advantages-block -->
-              <?php $i++; endwhile; endif; ?>
-
-              <div class="clearfix"></div>
-            </div>
-            <!-- end about-us__advantages-wrap -->
-          </div>
-        </div>
-      </div>
-      <!-- end about-us__advantages -->
-    </section>
-    <!-- end section.about-us -->
-    <section class="about-us__gallery">
-      <div class="container">
-        <div class="row">
-          <div class="section-tittle--left">
-            <h2>ФОТОГАЛЕРЕЯ</h2>
-            <ul class="nav nav-tabs nav-tabs--gallery">
-              <li class="active"><a href="#interior" data-toggle="tab">ИНТЕРЬЕР</a></li>
-              <li><a href="#apparatus" data-toggle="tab">ТРЕНАЖЕРЫ</a></li>
-            </ul>
-          </div>
-          <!-- end section-tittle--left -->
-        </div>
-      </div>
-      <!-- end container -->
-      <div class="tab-content gallery">
-        <div class="tab-pane fade in active" id="interior">
-          <div class="gallery__images">
-
-            <?php if( have_rows('gallery_first_block') ): while ( have_rows('gallery_first_block') ) : the_row();?>
-              <?php $image = get_sub_field('images'); if( !empty($image) ): ?>
-                <div class="col-xs-6 col-md-2 gallery__images-block">
-                  <a href="<?php echo $image['url']; ?>" data-toggle="lightbox" data-gallery="example-gallery">
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-fluid">
-                  </a>
                 </div>
-              <?php endif; ?>
+                <!-- end shedule__slider-descr-wrap -->
+                <div class="col-sm-6 col-md-4 shedule__slider-teacher-wrap">
+                  <div class="shedule__slider-teacher">
+                    <div class="shedule__slider-teacher-img">
+                      <?php $image = get_sub_field('teacher_photo'); ?>
+                      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                    </div>
+                    <!-- end shedule__slider-teacher-img -->
+                    <div class="shedule__slider-teacher-text">
+                      <h3><?php the_sub_field('teacher_name'); ?></h3>
+                      <p class="name"><?php the_sub_field('teacher_post'); ?></p>
+                      <?php the_sub_field('teacher_description'); ?>
+                    </div>
+                    <!-- end shedule__slider-teacher-text -->
+                  </div>
+                </div>
+                <!-- end shedule__slider-teacher-wrap -->
+                <div class="clearfix"></div>
+              </div><!-- end shedule__slider-block -->
             <?php  endwhile; endif; ?>
 
-            <div class="clearfix"></div>
-          </div><!-- end gallery__images -->
-        </div>
-        <div class="tab-pane fade" id="apparatus">
-          <div class="gallery__images">
-
-            <?php if( have_rows('gallery_second') ): while ( have_rows('gallery_second') ) : the_row();?>
-              <?php $image = get_sub_field('image'); if( !empty($image) ): ?>
-                <div class="col-xs-6 col-md-2 gallery__images-block">
-                  <a href="<?php echo $image['url']; ?>" data-toggle="lightbox" data-gallery="example-gallery">
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-fluid">
-                  </a>
-                </div>
-              <?php endif; ?>
-            <?php  endwhile; endif; ?>
-
-            <div class="clearfix"></div>
-          </div>
-          <!-- end gallery__images -->
-        </div>
-        <div class="gallery__descr-wrap"></div>
-      </div>
-    </section>
-    <!-- end about-us__gallery -->
-    <section class="about-us__personal">
-      <div class="container">
-        <div class="row">
-          <div class="section-tittle--left">
-            <h2>КОМАНДА</h2>
-            <ul class="nav nav-tabs nav-tabs--gallery nav-tabs--personal">
-              <li class="active"><a href="#instructors" data-toggle="tab">ИНСТРУКТОРЫ</a></li>
-              <li><a href="#administration" data-toggle="tab">АДМИНИСТРАЦИЯ</a></li>
-            </ul>
-          </div>
-          <!-- end section-tittle--left -->
-          <div class="tab-content personal">
-            <div class="tab-pane fade in active" id="instructors">
-              <div class="slider-personal">
-
-
-                <?php if( have_rows('instructors') ): while ( have_rows('instructors') ) : the_row();?>
-                  <?php $image = get_sub_field('image'); if( !empty($image) ): ?>
-                    <div class="slider-personal__block">
-                      <div class="slider-personal__img">
-                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-                      </div>
-                      <div class="slider-personal__text">
-                        <h3><?php the_sub_field('name'); ?></h3>
-                        <p><?php the_sub_field('position'); ?></p>
-                      </div>
-                    </div><!-- end slider-personal__block -->
-
-                  <?php endif; ?>
-                <?php  endwhile; endif; ?>
-
-              </div>
-              <!-- end slider-personal -->
+          </div><!-- end shedule__slider -->
+          <div class="shedule__table-wrap">
+            <div class="col-md-3">
+              <h2>РАСПИСАНИЕ ВСЕХ ГРУППОВЫХ ЗАНЯТИЙ</h2>
             </div>
-            <div class="tab-pane fade" id="administration">
-              <div class="slider-personal">
-
-                <?php if( have_rows('administration') ): while ( have_rows('administration') ) : the_row();?>
-                  <?php $image = get_sub_field('image'); if( !empty($image) ): ?>
-                    <div class="slider-personal__block">
-                      <div class="slider-personal__img">
-                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-                      </div>
-                      <div class="slider-personal__text">
-                        <h3><?php the_sub_field('name'); ?></h3>
-                        <p><?php the_sub_field('position'); ?></p>
-                      </div>
-                    </div><!-- end slider-personal__block -->
-
-                  <?php endif; ?>
-                <?php  endwhile; endif; ?>
-
+            <div class="col-md-9">
+              <div class="table-responsive">
+                <table class="table shedule__table">
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>ПН</th>
+                      <th>ВТ</th>
+                      <th>СР</th>
+                      <th>ЧТ</th>
+                      <th>ПТ</th>
+                      <th>СБ</th>
+                      <th>ВС</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>9:00</td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>11:00</td>
+                      <td></td>
+                      <td></td>
+                      <td><span class="green">90x60x90</span></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td><span class="brown">TABATA</span></td>
+                    </tr>
+                    <tr>
+                      <td>13:00</td>
+                      <td><span class="pink">STRETCH + PILATES</span></td>
+                      <td></td>
+                      <td><span class="green">90x60x90</span></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td><span class="brown">TABATA</span></td>
+                    </tr>
+                    <tr>
+                      <td>14:00</td>
+                      <td><span class="pink">STRETCH + PILATES</span></td>
+                      <td></td>
+                      <td><span class="green">90x60x90</span></td>
+                      <td></td>
+                      <td><span class="blue">TOTAL BODY</span></td>
+                      <td></td>
+                      <td><span class="brown">TABATA</span></td>
+                    </tr>
+                    <tr>
+                      <td>16:00</td>
+                      <td></td>
+                      <td></td>
+                      <td><span class="green">90x60x90</span></td>
+                      <td></td>
+                      <td><span class="brown">TABATA</span></td>
+                      <td></td>
+                      <td><span class="pink">STRETCH + PILATES</span></td>
+                    </tr>
+                    <tr>
+                      <td>18:00</td>
+                      <td><span class="pink">STRETCH + PILATES</span></td>
+                      <td></td>
+                      <td><span class="green">90x60x90</span></td>
+                      <td></td>
+                      <td><span class="blue">TOTAL BODY</span></td>
+                      <td></td>
+                      <td><span class="brown">TABATA</span></td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-              <!-- end slider-personal -->
+              <!-- end table-responsive -->
             </div>
+            <div class="clearfix"></div>
           </div>
-          <!-- end personal -->
+          <!-- end shedule__table -->
         </div>
+        <!-- end section-body -->
       </div>
-    </section><!-- end about-us__gallery -->
+    </div>
+  </section><!-- end section.shedule -->
 
     <?php /*
     <section class="partners">
